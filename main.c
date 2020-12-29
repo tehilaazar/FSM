6128a118c302b0425ba7c86507469d7d8ccb7aef
tehilaazar <51374908+tehilaazar@users.noreply.github.com>
2020-12-28 22:15:57 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -27,8 +27,10 @@ struct State {
 int getNumOfLetter(char letter){
     if (letter <= 'z' && letter >= 'a')
         return letter - 97;
-    else
+    else if(letter <= 'Z' && letter >= 'A')
         return letter - 39;
+    else
+        return -1;
 }
 
 // counts the number of lines in the file
@@ -59,13 +61,66 @@ bool checkElem(struct State arr[], int elem, int size ) {
         }
         return 0;
 }
+// Looping through list of structs of arrInputStructs to make
+// sure that the "nextState" values are possible states in the FSM
+bool checkNextStateValues(int numLines, struct State arrOfStates[], struct InputNextState arrInputStructs[], int numStates){
+    for (int i = 0; i < numLines; i++ ){
+        bool stateExists = false;
+        for (int j = 0; j < numStates; j++){
+            printf("arrOfStates[y].name is %d and arrInputStructs[z].nextState is %d\n", arrOfStates[j].name, arrInputStructs[i].nextState);
+            if (arrInputStructs[i].nextState == arrOfStates[j].name) {
+                stateExists = true;
+                break;
+            }
+        }
+        // if one of the nextState values from arrInputStructs is not a valid state, will return false
+        if (stateExists == false) {
+            return false;
+        }
+    }
+    return true;
+
+}
+
+// makes sure FSM has state zero
+bool checkStateZero(int numStates, struct State arrOfStates[]){
+    for (int j=0; j<numStates; j++){
+        if (arrOfStates[j].name == 0)
+            return true;
+    }
+    return false;
+}
+
+// Sets in the array of inputs for each state a default value of -1
+// since that is not a valid state number.
+// This will make it easier to check when get the inputs
+// whether the input is valid and can go to another state.
+void setDefaultNextStateVal(int numStates, struct State arrOfStates[]){
+    for (int i=0; i<numStates; i++){
+        for (int j = 0; j < 52; j++){
+            arrOfStates[i].inputs[j] = -1;
+        }
+    }
+}
+
+// Adds all the information from the arrInputStructs into the State structs
+void fillStructStates(int numLines, struct InputNextState arrInputStructs[], int numStates, struct State arrOfStates[]){
+    for (int i=0; i<numLines; i++){
+        int s = arrInputStructs[i].state;
+        for (int j=0; j<numStates; j++){
+            if (s == arrOfStates[j].name){
+                int numChar = getNumOfLetter(arrInputStructs[i].input);
+                arrOfStates[j].inputs[numChar] = arrInputStructs[i].nextState;
+                printf("Now state %d has in its list of inputs[%d] the value  %d\n", s, numChar, arrInputStructs[i].nextState);
+                break;
+            }
+        }
+    }
+}
+
 
 // creates the FSM simulation
 bool createFSM(char *defsFile, struct State arrOfStates[], int *numStates) {
-
-    //get full file path for the file arguments
-    //char defsFile[50] = "/home/tazar/CLionProjects/FSM/";
-    //strcat(defsFile, defs);
 
     // get number of lines of file which is number of inputs > nextStates for test1.fsm
     int numLines = countLines(defsFile);
@@ -120,44 +175,20 @@ bool createFSM(char *defsFile, struct State arrOfStates[], int *numStates) {
     }
     fclose(fsmDef);
 
-    // Looping through list of structs of arrInputStructs to make
-    // sure that the "nextState" values are possible states in the FSM
-    for (int z = 0; z < numLines; z++ ){
-        bool stateExists = false;
-        for (int y = 0; y < *numStates; y++){
-            if (arrInputStructs[z].nextState == arrOfStates[y].name) {
-                stateExists = true;
-                break;
-            }
-        }
+    int numStatesVal = *numStates;
 
-        // if one of the nextState values from arrInputStructs is not a valid state, will cause an error
-        if (stateExists == false) {
-            printf("ERROR: The next state %d is not a valid state in the FSM\n", arrInputStructs[z].nextState);
+    // check to see if any "nextState" value is not considered a valid state
+    bool stateExists = checkNextStateValues(numLines, arrOfStates, arrInputStructs, numStatesVal);
+    if (stateExists == false) {
+            printf("ERROR: One of the next state values is not a valid state in the FSM\n");
             return false;
         }
 
-    }
+    // Sets the default nextState for inputs array in the state structs to -1
+    setDefaultNextStateVal(numStatesVal, arrOfStates);
 
-    /*
-    Sets in the array of inputs for each state a default value of -1
-    since that is not a valid state number.
-    This will make it easier to check when get the inputs
-    whether the input is valid and can go to another state.
-
-    This also makes sure there is a state of zero.
-     */
-
-    bool stateZero = false;
-
-    for (int j=0; j<*numStates; j++){
-        if (arrOfStates[j].name == 0) {
-            stateZero = true;
-        }
-        for (int t = 0; t<52; t++){
-            arrOfStates[j].inputs[t] = -1;
-        }
-    }
+    // Checks if there is a state of zero
+    bool stateZero = checkStateZero(numStatesVal, arrOfStates);
 
     // If no state zero, not valid FSM
     if (stateZero == false){
@@ -165,28 +196,14 @@ bool createFSM(char *defsFile, struct State arrOfStates[], int *numStates) {
         return false;
     }
 
-    // Now adds all the information from the arrInputStructs into the State structs
-    for (int k=0; k<numLines; k++){
-        int s = arrInputStructs[k].state;
-        for (int x=0; x<*numStates; x++){
-            if (s == arrOfStates[x].name){
-                int numChar = getNumOfLetter(arrInputStructs[k].input);
-                arrOfStates[x].inputs[numChar] = arrInputStructs[k].nextState;
-                printf("Now state %d has in its list of inputs[%d] the value  %d\n", s, numChar, arrInputStructs[k].nextState);
-                break;
-            }
-        }
-    }
+    // Adds all the information from the arrInputStructs into the State structs
+    fillStructStates(numLines, arrInputStructs, numStatesVal, arrOfStates);
+
     return true;
 }
 
 // Now uses the FSM simulation to detect the final state after list of inputs
 bool useFSM(char *inputsFile, struct State arrOfStates[], int *numStates) {
-
-    // get full file path for the file arguments
-    //char inputsFile[50] = "/home/tazar/CLionProjects/FSM/";
-    //strcat(inputsFile, inputs);
-    printf("%s", inputsFile);
 
     FILE *fsmInputs = fopen(inputsFile, "r");
 
@@ -222,26 +239,24 @@ bool useFSM(char *inputsFile, struct State arrOfStates[], int *numStates) {
 
     fclose(fsmInputs);
     printf("after %d steps, state machine finished successfully at state %d", step, myState);
+    return true;
 }
-
 
 int main(int argc, char* argv[]) {
 
     if (argc != 3){
         printf("ERROR: incorrect number of arguments %d\n", argc);
     }
-    //printf("%d", getNumOfLetter('z'));
-    struct State arrOfStates[50];
-    int numStates;
-    bool fsm = createFSM(argv[1], arrOfStates, &numStates);
-    printf("created it.");
-    printf("the number of states are: %d\n", numStates);
+    else {
+        struct State arrOfStates[50];
+        int numStates;
+        bool fsm = createFSM(argv[1], arrOfStates, &numStates);
+        printf("created it.");
+        printf("the number of states are: %d\n", numStates);
 
-    if (fsm == true)
-        useFSM(argv[2], arrOfStates, &numStates);
-
-
-    //testGetNumOfLetter();
+        if (fsm == true)
+            useFSM(argv[2], arrOfStates, &numStates);
+    }
 }
 
 
